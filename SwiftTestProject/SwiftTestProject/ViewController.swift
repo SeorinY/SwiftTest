a5eed51485d4edb3739c295838acc829cd2a4f91
유서린 <sprout3082@gmail.com>
2022-10-20 15:24:08 +0900

    [FEA] :  Create Project

---
+++ SwiftTestProject/SwiftTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTestProject
+//
+//  Created by Mac Book Pro on 2022/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
