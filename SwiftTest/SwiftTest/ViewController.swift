767e3177450e06cb917f9437b0df67f993033c9c
유서린 <sprout3082@gmail.com>
2022-12-13 01:11:16 +0900

    [Chore] - project 생성

---
+++ SwiftTest/SwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Mac Book Pro on 2022/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- SwiftTestProject/Pods/Target Support Files/Pods-SwiftTestProject/Pods-SwiftTestProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftTestProject : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftTestProject
-@end
